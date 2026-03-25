7b6e040f51dc1428dc0b539f90cc741fc811f3bd
Miguel Chamorro <miguelchamorro912@gmail.com>
2026-03-25 19:58:21 -0300

    stable build 1.0

--- src/app/update-password/page.jsx
+++ src/app/update-password/page.jsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { updatePassword } from '@/app/actions/auth'
 
 export default function UpdatePasswordPage() {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  document.title = 'Actualizar contraseña | VanCheck';
+
+  useEffect(() => {
+    document.title = 'Actualizar contraseña | VanCheck';
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault()
